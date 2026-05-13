142157f29d7fc205c2db725554019cd495456e2a
Kant <xiaolexl520@gmail.com>
2026-05-13 14:55:03 -0700

    fix: correct App.css import case for Linux builds

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "./app.css";
+import "./App.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
