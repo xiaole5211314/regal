ff6d1dcf15a2ba08ff7f158645f6c7e1f1256c0c
Kant <xiaolexl520@gmail.com>
2026-05-13 14:27:58 -0700

    setup GitHub Pages auto-deploy via Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/regal/",
     server: {
         proxy: {
             "/api": "http://localhost:3001"
